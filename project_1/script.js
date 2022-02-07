a2567f739421212dd06d8c1ad5c5a3471be929c4
osmitee <obers735@newschool.edu>
2022-02-07 02:20:00 -0500

    project1

--- project_1/script.js
+++ project_1/script.js
@@ -2,3 +2,16 @@ function clickButton() {
     let button = document.getElementById("button");
     button.style.opacity = "0%";
 }
+
+function clickLever() {
+    let lever = document.getElementById("lever");
+    let leverDown = document.getElementById("leverdown");
+    lever.style.opacity = "0%";
+    leverDown.style.opacity = "100%";
+}
+
+<script>
+ $( ".et-pb-icon" ).click(function() {
+ $( this ).toggleClass( "active" );
+ });
+</script>
