a47424bdd8fe9bf25ae5cb0af36d0cc76d334545
osmitee <obers735@newschool.edu>
2022-03-07 11:11:21 -0500

    project2

---
+++ project_2/msg.js
@@ -0,0 +1,28 @@
+var circleOne = document.getElementById("circle-one")
+var circleTwo = document.getElementById("circle-two")
+var circleThree = document.getElementById("circle-three")
+var circleFour = document.getElementById("circle-four")
+var circleFive = document.getElementById("circle-five")
+
+window.onscroll = function () {
+    myFunction()
+};
+
+
+function myFunction() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+      circleOne.style.top = "400px";
+      circleTwo.style.transform =  "rotate(-154deg)";
+      circleTwo.style.height = "100px";
+      circleThree.style.height = "154px";
+      circleThree.style.top = "620px";
+      circleThree.style.left = "-55px";
+      circleFour.style.left = "270px";
+      circleFour.style.top = "450px";
+         circleFive.style.top = "100px";
+
+      
+
+      
+  }
+}
