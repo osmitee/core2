93992bd4d9c300ec550467b5547dfe70d22966bf
osmitee <obers735@newschool.edu>
2022-04-18 01:15:09 -0400

    js

--- project_3/wandavision.js
+++ project_3/wandavision.js
@@ -20,4 +20,33 @@ fetch('https://api.airtable.com/v0/appGeugv5v8JANFJa/wandavision', {
 });
     });
 
+// function episodes() {
+//     var episodes = document.getElementById('episodes');
+//     episodes.style.left = '208px';
+//     episodes.style.top = '197px';
+//     episodes.style.filter = 'brightness(130%)';
+//     var fifties = document.getElementById('ep-50s');
+//     fifties.style.visibility = 'visible';
+// }
+
+
+function episodes() {
+    var episodes = document.getElementById('episodes');
+    if (episodes.style.left = '172px') {
+        episodes.style.left = '208px'
+    }
+    else {
+        episodes.style.left = '172px';
+    }
+    var fifties = document.getElementById('ep-50s');
+    fifties.style.visibility = 'visible';
+}
+
+
  
+function characters() {
+    var characters = document.getElementById('characters');
+    characters.style.left = '208px';
+    characters.style.top = '197px';
+    characters.style.filter = 'brightness(130%)';
+}
